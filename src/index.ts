8e99ef42bd4df5de681c3b092dfab838d4113f8a
Christopher Juwao <njuwao@icloud.com>
2024-06-11 20:39:25 +0200

    implement security deps

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import costCheckerRoutes from "./cost-checker/routes";
 
+dotenv.config();
+
 const app = express();
-const port = 13000;
+const port = process.env.PORT || 13000;
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript with Express!");
