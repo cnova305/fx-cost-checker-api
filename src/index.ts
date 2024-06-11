5051d786a3573b0eac4d87c713e81f56ed34d408
Christopher Juwao <njuwao@icloud.com>
2024-06-11 17:21:23 +0200

    fix router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+import cors from "cors";
 import express from "express";
+import costCheckerRoutes from "./cost-checker/routes";
 
 const app = express();
 const port = 13000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript with Express!");
 });
+
+app.use("/api/", costCheckerRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
