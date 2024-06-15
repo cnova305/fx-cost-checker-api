50ede47606e224ac8df13a5bd8b0ffae83d8bb48
Christopher Juwao <njuwao@icloud.com>
2024-06-15 15:06:04 +0200

    Creat Client

--- functions/src/clients/create-client.ts
+++ functions/src/clients/create-client.ts
@@ -1,1 +1,22 @@
-// export const createClient = onRequest((request, response) => {});
+import * as admin from "firebase-admin";
+import { onRequest } from "firebase-functions/v2/https";
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const createClient = onRequest(async (req, res) => {
+  const data = req.body;
+
+  if (!data) {
+    res.status(400).send("No data provided");
+    return;
+  }
+  try {
+    const docRef = await db.collection("clients").add(data);
+    res.status(200).send(`Document added with ID: ${docRef.id}`);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    res.status(500).send("Error adding document");
+  }
+});
